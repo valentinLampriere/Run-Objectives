56a163e8a23ee52c798f138531f2f1f678c73b43
Valentin Lamprière <valentin.lampriere1@gmail.com>
2022-11-06 21:10:03 +0100

    Implement Quality Four Reroller reward

--- scripts/objectives/quality_four_reroller.lua
+++ scripts/objectives/quality_four_reroller.lua
@@ -1,10 +1,24 @@
 local RerollQualityFourObjective = { }
 
 local itemConfig = Isaac.GetItemConfig()
+local itemPool = Game():GetItemPool()
 local d6frameUsed = 0
 local hasRerolledQuality4 = false
 
 local quality4RerollerObjective = RunObjectivesAPI.Objective:new(RerollQualityFourObjective, "Quality 4 reroller")
+
+local game = Game()
+local rng
+
+RerollQualityFourObjective.Stats = {
+    RerollChance = {
+        [0] = 90,
+        [1] = 75,
+        [2] = 50,
+        [3] = 25,
+        [4] = 0
+    }
+}
 
 function RerollQualityFourObjective:Evaluate()
     return hasRerolledQuality4
@@ -36,13 +50,52 @@ function RerollQualityFourObjective:PreUseD6_uncomplete(collectibleType, rng, pl
         end
     end
 end
-function RerollQualityFourObjective:PreUseD6_complete(collectibleType, rng, player, flags, slot)
-    d6frameUsed = Game():GetFrameCount()
+
+function RerollQualityFourObjective:PreUseD6_complete(collectibleType, _rng, player, flags, slot)
+    d6frameUsed = game:GetFrameCount()
+    rng = _rng
 end
 
-function RerollQualityFourObjective:OnPickupSelection(pickup, variant, subtype)
-    if variant == PickupVariant.PICKUP_COLLECTIBLE and d6frameUsed == Game():GetFrameCount() then
-        return { variant, 8 }
+local function GetPoolType(seed)
+    local roomType = game:GetRoom():GetType()
+    local poolType = itemPool:GetPoolForRoom(roomType, seed)
+    if poolType == -1 then poolType = ItemPoolType.POOL_TREASURE end
+    return poolType
+end
+
+local function TryGetBetterItem(poolType, seed, collectible)
+    local quality = GetCollectibleQuality(collectible)
+    local chance = RerollQualityFourObjective.Stats.RerollChance[quality]
+    local roll = rng:RandomFloat() * 100
+
+    if roll < chance then
+        local newCollectible = itemPool:GetCollectible(poolType, false, seed)
+        local newQuality = GetCollectibleQuality(newCollectible)
+
+        if newQuality > quality then
+            return TryGetBetterItem(poolType, seed, newCollectible)-- or newCollectible
+        else
+            return TryGetBetterItem(poolType, seed, collectible)
+        end
+    end
+
+    return collectible
+end
+
+function RerollQualityFourObjective:TryGetBetterItemOnPickupSelection(pickup, variant, subtype)
+    if variant ~= PickupVariant.PICKUP_COLLECTIBLE or d6frameUsed ~= game:GetFrameCount() then
+        return
+    end
+
+    local seed = rng:GetSeed()
+    local poolType = GetPoolType(seed)
+
+    local newCollectible = TryGetBetterItem(poolType, seed, subtype)
+
+    if newCollectible ~= subtype then
+        itemPool:RemoveCollectible(newCollectible)
+        print("Reroll " .. subtype .. " to " .. newCollectible)
+        return { variant, newCollectible }
     end
 end
 
@@ -50,4 +103,4 @@ end
 RunObjectivesAPI:AddObjectiveCallback(quality4RerollerObjective, false, ModCallbacks.MC_PRE_USE_ITEM, RerollQualityFourObjective.PreUseD6_uncomplete, CollectibleType.COLLECTIBLE_D6)
 RunObjectivesAPI:AddObjectiveCallback(quality4RerollerObjective, true, ModCallbacks.MC_PRE_USE_ITEM, RerollQualityFourObjective.PreUseD6_complete, CollectibleType.COLLECTIBLE_D6)
 -- MC_POST_PICKUP_SELECTION --
-RunObjectivesAPI:AddObjectiveCallback(quality4RerollerObjective, true, ModCallbacks.MC_POST_PICKUP_SELECTION, RerollQualityFourObjective.OnPickupSelection)
+RunObjectivesAPI:AddObjectiveCallback(quality4RerollerObjective, true, ModCallbacks.MC_POST_PICKUP_SELECTION, RerollQualityFourObjective.TryGetBetterItemOnPickupSelection)
